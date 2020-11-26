28a806dc34b2dd4f4f97c7c1e73569c69da091ab
Martin Lowe <martin.lowe@eclipse-foundation.org>
2020-11-26 15:26:23 -0500

    Update project structure to be a combined application
    Signed-off-by: Martin Lowe <martin.lowe@eclipse-foundation.org>

---
+++ src/main/java/org/eclipsefoundation/react/request/OIDCResource.java
@@ -0,0 +1,75 @@
+package org.eclipsefoundation.react.request;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.eclipsefoundation.core.model.RequestWrapper;
+import org.eclipsefoundation.core.namespace.UrlParameterNamespace.UrlParameter;
+import org.eclipsefoundation.react.helper.CSRFHelper;
+import org.eclipsefoundation.react.model.AdditionalUserData;
+
+import io.quarkus.security.identity.SecurityIdentity;
+
+/**
+ * Handles OIDC routing for the request.
+ *
+ * @author Martin Lowe
+ */
+@Path("")
+public class OIDCResource {
+  @Inject CSRFHelper csrfHelper;
+  @Inject AdditionalUserData aud;
+  @Inject RequestWrapper wrap;
+  @Inject SecurityIdentity ident;
+
+  @GET
+  @RolesAllowed("")
+  @Path("/login")
+  public Response routeLogin() throws URISyntaxException {
+    return redirect("/");
+  }
+
+  /**
+   * While OIDC plugin takes care of actual logout, a route is needed to properly reroute anon user
+   * to home page.
+   *
+   * @throws URISyntaxException
+   */
+  @GET
+  @Path("/logout")
+  public Response routeLogout() throws URISyntaxException {
+    return redirect("/");
+  }
+
+  @GET
+  @Path("userinfo")
+  public Response getUserInfo() {
+    if (!ident.isAnonymous()) {
+      return Response.ok(ident.getPrincipal().toString()).build();
+    } else {
+      return Response.ok().build();
+    }
+  }
+
+  @GET
+  @Path("csrf")
+  public Response generateCSRF() {
+    if (!ident.isAnonymous()) {
+      aud.setCsrf(csrfHelper.getNewCSRFToken());
+      wrap.setParam(new UrlParameter("csrf"), aud.getCsrf());
+      return Response.ok().build();
+    } else {
+      return Response.status(403).build();
+    }
+  }
+
+  private Response redirect(String location) throws URISyntaxException {
+    return Response.temporaryRedirect(new URI(location)).build();
+  }
+}
