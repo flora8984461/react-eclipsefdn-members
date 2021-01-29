6891bd824c8b93f0e9ffc74e259dd1e83bf0166c
Yi Liu <yi.liu@eclipse-foundation.org>
2021-01-29 17:15:29 -0500

    spacing for company info
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

--- src/components/forms/CompanyInformation.js
+++ src/components/forms/CompanyInformation.js
@@ -25,7 +25,7 @@ const CompanyInformation = ({ formField, label, skipped, mktSame, setMktSame, ac
       <h2 className="fw-600">Company Information</h2>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
-      <div className="">
+      <div className="align-center">
       <h4 className="fw-600"> Organization * </h4>
       <CustomSelectWrapper
         name="organization.legalName"
