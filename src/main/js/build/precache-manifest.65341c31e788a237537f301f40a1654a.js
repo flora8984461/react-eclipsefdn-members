499ef842c30abc57f1e19cc574c56cdfda207990
Yi Liu <yi.liu@eclipse-foundation.org>
2021-02-18 13:40:16 -0500

    Temp save the local settings for Yi
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>

---
+++ src/main/js/build/precache-manifest.65341c31e788a237537f301f40a1654a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ff14fe3bfeabe68cd52ff28e917642e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c987c99a4edc6ba31365",
+    "url": "/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "21a8817c4dd99fb6215d",
+    "url": "/static/js/2.c5432127.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c5432127.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c987c99a4edc6ba31365",
+    "url": "/static/js/main.aed87ce1.chunk.js"
+  },
+  {
+    "revision": "5798ac16fb5c1bc519b9",
+    "url": "/static/js/runtime-main.42d6bf63.js"
+  }
+]);
